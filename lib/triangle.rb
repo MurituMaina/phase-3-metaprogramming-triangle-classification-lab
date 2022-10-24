b3fcf1fd0032cdbdcd13ec20cff8f4ff15067c82
MurituMaina <vincent.maina@student.moringaschool.com>
2022-10-24 14:45:18 +0300

    commit tree 8ef03095659909de259497092cfbb25af1c37d02

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,8 +7,7 @@ class Triangle
     @side3 = side3
   end
   def kind
-if   @side1 > 0 && @side2 > 0 && @side3 > 0 &&  @side1 + @side2 > @side3 && @side1 + @side3 >  @side2 && @side2 + @side3 > @side1
-
+    triangle_validate
       if @side1 == @side2 && @side2 == @side3
         :equilateral
       elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
@@ -16,9 +15,23 @@ if   @side1 > 0 && @side2 > 0 && @side3 > 0 &&  @side1 + @side2 > @side3 && @sid
       else
         :scalene
       end
-  else
-    raise TriangleError;
+  # else
+  #   raise TriangleError;
+  # end
   end
+
+
+  def  side_equal_zero?
+ @side1 > 0 && @side2 > 0 && @side3 > 0
+  end
+
+  def triangle_inequality?
+     @side1 + @side2 > @side3 && @side1 + @side3 >  @side2 && @side2 + @side3 > @side1
+
+  end
+
+  def triangle_validate
+  raise TriangleError unless triangle_inequality? && side_equal_zero?
   end
   class TriangleError < StandardError
   
